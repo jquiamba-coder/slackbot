0b872ad6ef36b42461b01e49087cea0c3bd380ea
jquiamba-coder <84034461+jquiamba-coder@users.noreply.github.com>
2021-06-08 23:27:13 +0800

    Add files via upload

---
+++ slackbot.py
@@ -0,0 +1,167 @@
+import slack
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from flask import Flask, request, Response
+from slackeventsapi import SlackEventAdapter
+import string
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+app = Flask(__name__)
+slack_event_adapter = SlackEventAdapter(
+    os.environ['SIGNING_SECRET'],'/slack/events', app)
+
+client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+BOT_ID = client.api_call("auth.test")['user_id']
+message_counts = {}
+welcome_messages = {}
+
+KEY_WORDS1 = ['2fa', 'Okta 2fa', 'reset 2fa']
+KEY_WORDS2 = ['Password', 'Okta lockout', 'reset password']
+KEY_WORDS3 = ['Slack', 'Slack Channel', 'Add to channel']
+
+class WelcomeMessage:
+    START_TEXT = {
+        'type': 'section',
+        'text': {
+            'type': 'mrkdwn',
+            'text': (
+                ':tada: Welcome to the #IT-Support channel! :tada: \n\n'
+                ' This is your IT SlackBot at your Service :robot_face:'
+            )
+        }
+    }
+
+    DIVIDER = {'type': 'divider'}
+
+    def __init__(self, channel, user):
+        self.channel = channel
+        self.user = user
+        self.icon_emoji = ':robot_face:'
+        self.timestamp = ''
+        self.completed = False
+
+
+    def get_message(self):
+        return {
+            'ts': self.timestamp,
+            'channel': self.channel,
+            'username': 'Welcome Robot!',
+            'icon_emoji': self.icon_emoji,
+            'blocks': [
+                self.START_TEXT,
+                self.DIVIDER,
+                self._get_reaction_task()
+            ]
+        }
+    
+    def _get_reaction_task(self):
+        checkmark = ':white_check_mark:'
+        if not self.completed:
+            checkmark = ':white_large_square:'
+
+        text = f'{checkmark} *React to this message so we know you are not a Bot!*'
+
+        return {'type': 'section', 'text': {'type': 'mrkdwn', 'text': text}}
+
+def send_welcome_message(channel, user,):
+        if channel not in welcome_messages:
+            welcome_messages[channel] = {}
+
+        if user in welcome_messages[channel]:
+            return
+
+        welcome = WelcomeMessage(channel, user)
+        message = welcome.get_message()
+        response = client.chat_postMessage(**message)
+        welcome.timestamp = response['ts']
+
+        welcome_messages[channel][user] = welcome
+
+def check_if_key_words1(message):
+    msg = message.lower()
+    msg = msg.translate(str.maketrans('', '', string.punctuation))
+    return any(word in msg for word in KEY_WORDS1)
+
+def check_if_key_words2(message):
+    msg = message.lower()
+    msg = msg.translate(str.maketrans('', '', string.punctuation))
+    return any(word in msg for word in KEY_WORDS2)
+
+def check_if_key_words3(message):
+    msg = message.lower()
+    msg = msg.translate(str.maketrans('', '', string.punctuation))
+
+    return any(word in msg for word in KEY_WORDS3)
+
+@slack_event_adapter.on('member_joined_channel')
+def join(payload):
+    event = payload.get('event', {})
+    channel_id = event.get('channel')
+    user_id = event.get('user')
+    text = event.get('text')
+
+    if user_id != None and BOT_ID != user_id:
+        send_welcome_message(f'@{user_id}', user_id)
+
+
+@slack_event_adapter.on('message')
+def message(payload):
+    event = payload.get('event', {})
+    channel_id = event.get('channel')
+    user_id = event.get('user')
+    text = event.get('text')
+
+    if user_id != None and BOT_ID != user_id:
+        if user_id in message_counts:
+            message_counts[user_id] += 1
+        else:
+            message_counts[user_id] = 1
+
+        if text.lower() == 'start':
+            send_welcome_message(f'@{user_id}', user_id)
+        elif check_if_key_words1(text):
+            ts=event.get('ts')
+            client.chat_postMessage(
+                channel=channel_id, thread_ts=ts, text ="Do you need help with 2fa?")
+
+        elif check_if_key_words2(text):
+            ts=event.get('ts')
+            client.chat_postMessage(
+                channel=channel_id, thread_ts=ts, text ="Do you need help with your Okta Password?")
+
+        elif check_if_key_words3(text):
+            ts=event.get('ts')
+            client.chat_postMessage(
+                channel=channel_id, thread_ts=ts, text ="Do you need help with your Okta Password?")
+
+@slack_event_adapter.on('reaction_added')
+def reaction(payload):
+    event = payload.get('event', {})
+    channel_id = event.get('item', {}).get('channel')
+    user_id = event.get('user')
+
+    if f'@{user_id}' not in welcome_messages:
+        return
+    
+    welcome = welcome_messages[f'@{user_id}'][user_id]
+    welcome.completed = True
+    welcome.channel = channel_id
+    message = welcome.get_message()
+    updated_message = client.chat_update(**message)
+    welcome.timestamp = updated_message['ts']
+
+@app.route('/message-count', methods=['POST'])
+def message_count():
+    data = request.form
+    user_id = data.get('user_id')
+    channel_id = data.get('channel_id')
+    message_count = message_counts.get(user_id, 0)
+
+    client.chat_postMessage(channel=channel_id, text=f"message: {message_count}")
+    return Response(), 200
+
+if __name__ == "__main__":
+    app.run(debug=True)    
